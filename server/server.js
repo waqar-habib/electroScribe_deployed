3536766275e7a26878e6a30f6afda41e29a11268
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-08-12 12:43:43 -0500

    reuploading files since datasources broke

--- server/server.js
+++ server/server.js
@@ -12,8 +12,7 @@ var app = module.exports = loopback();
 
 app.start = function() {
   // start the web server
-  var PORT = process.env.PORT || 3000;
-  return app.listen(PORT, function() {
+  return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Browse electroScribe app at: %s', baseUrl);
