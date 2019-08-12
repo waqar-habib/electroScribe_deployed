e995b0c9c3e572c886f7b8c94d102d1cd5159aa9
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-08-12 11:34:42 -0500

    pushing build files

---
+++ client/precache-manifest.a5ede7b249c838fdb725f3b64210fc30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303e2db8114cd4631b0d1824d02c591a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "260faf0133a5ddf8a7d3",
+    "url": "/static/css/main.61e8e05a.chunk.css"
+  },
+  {
+    "revision": "5a8928cdd4e162668911",
+    "url": "/static/js/2.92a91973.chunk.js"
+  },
+  {
+    "revision": "260faf0133a5ddf8a7d3",
+    "url": "/static/js/main.8896775d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "14f52411d8fe0d1702b310cf624ac7c1",
+    "url": "/static/media/Bacteria.14f52411.mp4"
+  }
+]);
